b5fc7e23bee840349a881aab35d90dc21f3b9039
Valery <wogl@mail.ru>
2022-11-23 01:24:51 +0300

    1

---
+++ 25zadacha/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// 2 решение
+Console.WriteLine("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень, в которую хотите возвести: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int numbA = A;
+for (int i = 1; i < B; i++)
+{
+    A = numbA * A;
+}
+Console.WriteLine($"{numbA} в {B} степени равно: {A}");
+
+// 1 решение
+// Console.WriteLine("Введите число: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите степень, в которую хотите возвести: ");
+// int B = Convert.ToInt32(Console.ReadLine());
+// int result = Convert.ToInt32(Math.Pow(A, B));
+// Console.WriteLine(result);
---
+++ 27zadacha/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.WriteLine("Введите число: ");
+string number = Convert.ToString(Console.ReadLine());
+for (int i = 0; i <= number.Length; i++)
+{
+  Console.WriteLine(number.Length);
+}
+
